d79561ed12d26335b80f11e5def0f29846a46a91
Kyranus <kyran_Wolking@yahoo.com>
2025-04-20 21:37:53 +0200

    test

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+ class test {
+    public static void main(String[] args) {
+        Date datum = new Date(); // Dit geeft de huidige datum en tijd
+        System.out.println("Datum: " + datum);
+    }
+}
